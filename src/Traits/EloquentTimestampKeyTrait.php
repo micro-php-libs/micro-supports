80ecfe0495e8120e02d13722d4924645875f9a00
Ty Huynh <hongty.huynh@gmail.com>
2021-12-17 22:07:39 +0700

    add model trait key uuid or timestamp

---
+++ src/Traits/EloquentTimestampKeyTrait.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of DBCSoft Standard Package
+ *
+ * (c) Ty Huynh <hongty.huynh@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace MicroPhpLibs\MicroSupports\Traits;
+
+trait EloquentTimestampKeyTrait
+{
+    /**
+     * Boot function from Laravel.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = now()->timestamp;
+            }
+        });
+    }
+
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    /**
+     * Get the auto-incrementing key type.
+     *
+     * @return string
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
+}
